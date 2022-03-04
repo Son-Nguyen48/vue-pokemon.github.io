3394ddf987623216559b94dfc5d9c021dacddd23
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-03-04 23:25:05 +0700

    Css-continues

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <div id="header">
+    <div class="header">
       <a class="logo"></a>
       <input
         v:mode="pok"
@@ -11,9 +11,18 @@
       <a href="" class="search-icon"><i class="ti-search"></i></a>
     </div>
     <div id="list-items">
-      <li class="item" v-for="pokemon in pokemons" :key="pokemon.name">
+      <li
+        class="item"
+        v-for="pokemon in pokemons"
+        :key="pokemon.name"
+        v-wow="{ 'animation-name': 'bounceIn', 'animation-duration': '1s' }"
+      >
         <img :src="pokemon.img" alt="" class="img-item" />
-        <a href="">Pokemon {{ pokemon.name }}</a>
+        <a href="" class="pokemon-name"> {{ pokemon.name }}</a>
+        <!-- <p v-for="ty in pokemon.type" :key="ty" class="pokemon-type">
+          {{ ty }}
+        </p> -->
+        <PokemonType :types="pokemon.type" />
       </li>
     </div>
   </div>
@@ -21,13 +30,16 @@
 
 <script>
 import axios from "axios";
+import PokemonType from "@/views/Home/PokemonType.vue";
 
 export default {
   name: "HelloWorld",
+  components: {
+    PokemonType,
+  },
   props: {
     msg: String,
     // eslint-disable-next-line prettier/prettier
-    PokemonName: String
   },
   data() {
     return {
